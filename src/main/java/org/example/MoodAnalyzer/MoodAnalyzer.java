6f7ec280c0713b31178ef03240f0debe2674cddb
Skyrimwell <skyrimwell@gmail.com>
2024-05-27 16:44:44 +0300

    Добавлены Hashset фильтров Прописаны примеры позитивных/негативных слов

--- src/main/java/org/example/MoodAnalyzer/MoodAnalyzer.java
+++ src/main/java/org/example/MoodAnalyzer/MoodAnalyzer.java
@@ -3,8 +3,29 @@ package org.example.MoodAnalyzer;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MoodAnalyzer {
+
+    private static final Set<String> POSITIVE_WORDS = new HashSet<>();
+    private static final Set<String> NEGATIVE_WORDS = new HashSet<>();
+
+    static {
+        POSITIVE_WORDS.add("хороший");
+        POSITIVE_WORDS.add("прекрасный");
+        POSITIVE_WORDS.add("отличный");
+        POSITIVE_WORDS.add("замечательный");
+        POSITIVE_WORDS.add("радостный");
+        POSITIVE_WORDS.add("счастливый");
+
+        NEGATIVE_WORDS.add("плохой");
+        NEGATIVE_WORDS.add("ужасный");
+        NEGATIVE_WORDS.add("отвратительный");
+        NEGATIVE_WORDS.add("грустный");
+        NEGATIVE_WORDS.add("несчастный");
+        NEGATIVE_WORDS.add("тревожный");
+    }
 
     public static void main(String[] args) {
         String filePath = args[0];

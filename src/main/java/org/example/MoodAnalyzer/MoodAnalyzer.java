29c43c55ba74b36f61123106deba3b349555fb92
Skyrimwell <skyrimwell@gmail.com>
2024-05-27 16:28:41 +0300

    Добавлен файловый ввод

--- src/main/java/org/example/MoodAnalyzer/MoodAnalyzer.java
+++ src/main/java/org/example/MoodAnalyzer/MoodAnalyzer.java
@@ -1,4 +1,22 @@
 package org.example.MoodAnalyzer;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class MoodAnalyzer {
+
+    public static void main(String[] args) {
+        String filePath = args[0];
+        String content;
+
+        try {
+            content = new String(Files.readAllBytes(Paths.get(filePath)));
+        } catch (IOException e) {
+            System.out.println("Не удалось прочитать файл: " + e.getMessage());
+            return;
+        }
+
+
+    }
 }
